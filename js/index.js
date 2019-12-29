affe86bf40522def97c27082cc3e70de77765c0f
Gonzalo Vivas <gonzalo.mansini@gmail.com>
2019-12-29 19:02:30 -0300

    html index preview

---
+++ js/index.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $('.modal').modal();
+    $(".modal").width($(".modal").width());
+    $(".modal").height($(".modal").height());
+
+    $("#logo-nav").addClass("hide");
+
+    $('.customer-logos').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 3
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 2
+          }
+      }]
+  });
+});
+
+$(window).scroll(function(){
+    if($(window).scrollTop() > $("#hero").height() - 64){
+        // $("#logo-nav").removeClass("hide");
+        $("#nav-container").removeClass("hide");
+    }
+    else{
+        // $("#logo-nav").addClass("hide");
+        $("#nav-container").addClass("hide");
+    }
+})
+
+$("#btn-hero").click(function(e) {
+  e.preventDefault();
+  // $("#quienes-somos").animatedScroll({easing: "easeOutExpo"});
+  $('html, body').animate({
+    scrollTop: $("#quienes-somos").offset().top-100
+  }, 500);
+})
+
+$("#btn-nuestros-trabajos").hover(function(e) {
+  $("#img-nuestros-trabajos").addClass("jqhover");
+})
+$("#btn-nuestros-trabajos").mouseout(function(e) {
+  $("#img-nuestros-trabajos").removeClass("jqhover");
+})
